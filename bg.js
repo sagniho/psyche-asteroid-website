6d33a7250dfd7bb8883578c0892f90e4eac38465
sagniho <sagniho1@asu.edu>
2023-02-26 14:51:42 -0700

    updates

--- bg.js
+++ bg.js
@@ -1,4 +1,6 @@
 //updates
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
